4ed53b299e7de13924abd23f814f8cef9a016b8a
daeunleeeee <lde7953@gmail.com>
2025-07-08 00:24:33 +0900

    feat#1: 로그인, 회원가입 페이지 ui 개발

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,19 @@
-import { Routes, Route } from 'react-router-dom';
-import { Suspense, lazy } from 'react';
-import Layout from './pages/_layout';
+import { useRoutes } from 'react-router-dom';
+import { routes } from './routes';
+import '@/styles/fonts.scss';
+import $ from './App.module.scss';
 
-const Login = lazy(() => import('./pages/login'));
-const Signup = lazy(() => import('./pages/signup'));
-const Main = lazy(() => import('./pages/main'));
-const Mypage = lazy(() => import('./pages/mypage'));
-const News = lazy(() => import('./pages/news'));
-const Test = lazy(() => import('./pages/test'));
-const Diary = lazy(() => import('./pages/diary'));
-const Vote = lazy(() => import('./pages/vote'));
-const Community = lazy(() => import('./pages/community'));
 
-const App = () => {
+function App() {
+  const elem = useRoutes(routes);
+
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Main />} />
-          <Route path="login" element={<Login />} />
-          <Route path="signup" element={<Signup />} />
-          <Route path="mypage" element={<Mypage />} />
-          <Route path="news" element={<News />} />
-          <Route path="test" element={<Test />} />
-          <Route path="diary" element={<Diary />} />
-          <Route path="vote" element={<Vote />} />
-          <Route path="community" element={<Community />} />
-        </Route>
-      </Routes>
-    </Suspense>
+    <div className={$.Wrapper}>
+    <div className={$.ContentWrapper}>
+        {elem}
+    </div>
+  </div>
   );
-};
+}
 
 export default App;
