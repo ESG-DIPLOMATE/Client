85503c0daba1fe1191fbbb8042f1922019bc4afe
daeunleeeee <lde7953@gmail.com>
2025-07-09 23:03:58 +0900

    feat#5: 앱바 컴포넌트 추가

--- src/pages/news/index.tsx
+++ src/pages/news/index.tsx
@@ -1,8 +1,12 @@
 import { useState } from "react";
 import $ from "./News.module.scss";
 import KeywordChip from "@/components/Chip/KeywordChip";
+import { useNavigate } from "react-router-dom";
+import AppBar from "@/components/common/Appbar";
 
 export default function News() {
+  const navigate = useNavigate();
+
   const typeName = "평화중재형";
 
   const keywords = ["전체", "ESG", "기후", "문화", "경제", "인권"];
@@ -13,43 +17,52 @@ export default function News() {
     setSelectedKeyword(keyword);
   };
 
+  const onBack = () => {
+    navigate(-1);
+  };
+
   return (
     <div className={$.wrapper}>
-      <header className={$.header}>
-        <h1 className={$.title}>외교뉴스</h1>
-      </header>
+      <div className={$.PaddingContainer}>
+        <AppBar leftRole="back" onClickLeftButton={onBack} />
+      </div>
+      <div className={$.container}>
+        <header className={$.header}>
+          <h1 className={$.title}>외교뉴스</h1>
+        </header>
 
-      <section className={$.section}>
-        <p className={$.subTitle}>
-          맞춤형 외교뉴스 -<span className={$.typeName}>{typeName}</span>
-        </p>
-        <div className={$.card}>
-          <div className={$.placeholderBox}></div>
-          <div className={$.placeholderBox}></div>
-        </div>
-      </section>
+        <section className={$.section}>
+          <p className={$.subTitle}>
+            맞춤형 외교뉴스 -<span className={$.typeName}>{typeName}</span>
+          </p>
+          <div className={$.card}>
+            <div className={$.placeholderBox}></div>
+            <div className={$.placeholderBox}></div>
+          </div>
+        </section>
 
-      <section className={$.section}>
-        <div className={$.newsHeader}>
-          <h2>외교뉴스 전체보기</h2>
-        </div>
-        <div className={$.keywordScroll}>
-          {keywords.map((keyword) => (
-            <KeywordChip
-              key={keyword}
-              label={keyword}
-              isActive={selectedKeyword === keyword}
-              onClick={() => handleKeywordClick(keyword)}
-            />
-          ))}
-        </div>
+        <section className={$.section}>
+          <div className={$.newsHeader}>
+            <h2>외교뉴스 전체보기</h2>
+          </div>
+          <div className={$.keywordScroll}>
+            {keywords.map((keyword) => (
+              <KeywordChip
+                key={keyword}
+                label={keyword}
+                isActive={selectedKeyword === keyword}
+                onClick={() => handleKeywordClick(keyword)}
+              />
+            ))}
+          </div>
 
-        <div className={$.card}>
-          <div className={$.placeholderBox}></div>
-          <div className={$.placeholderBox}></div>
-          <div className={$.placeholderBox}></div>
-        </div>
-      </section>
+          <div className={$.card}>
+            <div className={$.placeholderBox}></div>
+            <div className={$.placeholderBox}></div>
+            <div className={$.placeholderBox}></div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
--- src/pages/test/StartTest.tsx
+++ src/pages/test/StartTest.tsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import $ from "./StartTest.module.scss";
 import Button from "@/components/common/Button";
+import AppBar from "@/components/common/Appbar";
 
 export default function StartTest() {
   const navigate = useNavigate();
@@ -9,43 +10,53 @@ export default function StartTest() {
     navigate("/test");
   };
 
+  const onBack = () => {
+    navigate(-1);
+  };
+
   return (
     <div className={$.wrapper}>
-      <div className={$.contentWrapper}>
-        <section className={$.introSection}>
-          <h1>글로벌 시민력 테스트</h1>
-          <p>
-            나의 시민외교 성향을 알아보고
-            <br />
-            나에게 꼭 맞는 글로벌 참여 방법을 찾아보세요!
-          </p>
-        </section>
-
-        <section className={$.infoSection}>
-          <ul>
-            <li>
-              <strong>목적:</strong> 글로벌 이슈에 대한 나의 관심과 성향을
-              분석해 나에게 맞는 시민외교 활동을 추천합니다.
-            </li>
-            <li>
-              <strong>문항 수:</strong> 총 15문항 (지식, 태도, 성향이 고루 포함)
-            </li>
-            <li>
-              <strong>시민외교 타입:</strong> 평화중재형, 기후행동형,
-              문화외교형, 경제통상형, 디지털소통형
-            </li>
-            <li>
-              각 질문의 선택지가 특정 타입 점수에 영향을 주며, 최종적으로 가장
-              높은 점수의 타입을 알려드립니다.
-            </li>
-          </ul>
-        </section>
+      <div className={$.PaddingContainer}>
+        <AppBar leftRole="back" onClickLeftButton={onBack} />
       </div>
+      <div className={$.Container}>
+        {" "}
+        <div className={$.contentWrapper}>
+          <section className={$.introSection}>
+            <h1>글로벌 시민력 테스트</h1>
+            <p>
+              나의 시민외교 성향을 알아보고
+              <br />
+              나에게 꼭 맞는 글로벌 참여 방법을 찾아보세요!
+            </p>
+          </section>
 
-      <div className={$.buttonArea}>
-        <Button onClick={handleStart} variant="primary" size="large">
-          테스트 시작하기
-        </Button>
+          <section className={$.infoSection}>
+            <ul>
+              <li>
+                <strong>목적:</strong> 글로벌 이슈에 대한 나의 관심과 성향을
+                분석해 나에게 맞는 시민외교 활동을 추천합니다.
+              </li>
+              <li>
+                <strong>문항 수:</strong> 총 15문항 (지식, 태도, 성향이 고루
+                포함)
+              </li>
+              <li>
+                <strong>시민외교 타입:</strong> 평화중재형, 기후행동형,
+                문화외교형, 경제통상형, 디지털소통형
+              </li>
+              <li>
+                각 질문의 선택지가 특정 타입 점수에 영향을 주며, 최종적으로 가장
+                높은 점수의 타입을 알려드립니다.
+              </li>
+            </ul>
+          </section>
+        </div>
+        <div className={$.buttonArea}>
+          <Button onClick={handleStart} variant="primary" size="large">
+            테스트 시작하기
+          </Button>
+        </div>
       </div>
     </div>
   );
